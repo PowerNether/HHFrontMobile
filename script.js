11b504670f69d326d05dfcef80d329c2e416e4b8
PowerNether <64768544+PowerNether@users.noreply.github.com>
2021-04-22 15:39:14 +0500

    fix

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
 $(document).ready(function () {
     if ($(window).width() <= 768) {
         $('#tophead>.c-page').prepend($('header>.c-page>.logo'));
-        var toggle = $("<span class='toggle-menu' />")
+        var toggle = $('<div class="toggle-menu" />')
+            .append('<span class="toggle-icon" />')
         $('#tophead>.c-page').append(toggle);
-        var search = $("<span class='mobile-search' />")
+        var search = $('<span class="mobile-search" />')
         $('#tophead>.c-page').append(search);
-        var menu = $("<div class='mobile-menu' />")
-            .append("<div class='mobile-list'></div>")
+        var menu = $('<div class="mobile-menu" />')
+            .append('<div class="mobile-list"></div>')
         $('#tophead>.c-page').append(menu);
 
-        $(".mobile-menu").append($("<span class='toggle-menu active' />"))
-        $("#tophead>.c-page>.menu").clone().appendTo($(".mobile-menu"))
-        $("#tophead>.c-page>.region").clone().appendTo($(".mobile-menu"))
-        $(".mobile-list").append("<div class='mobile-categ'></div>")
-        $(".mobile-list").append("<div class='mobile-items'></div>")
-        $("header>.c-page>nav>ul>li").clone().appendTo($('.mobile-categ'));
+        $('.mobile-menu').append($('<span class="toggle-menu active" />'))
+        $('#tophead>.c-page>.menu').clone().appendTo($('.mobile-menu'))
+        $('#tophead>.c-page>.region').clone().appendTo($('.mobile-menu'))
+        $('.mobile-list').append('<div class="mobile-categ"></div>')
+        $('.mobile-list').append('<div class="mobile-items"></div>')
+        $('header>.c-page>nav>ul>li').clone().appendTo($('.mobile-categ'));
     } else {
         $('header>.c-page').prepend($('#tophead>.c-page>.logo'));
-        if ($("#tophead>.c-page").find(".mobile-menu").length > 0) {
-            $('#tophead>.c-page').remove(".mobile-menu");
-            $('#tophead>.c-page').remove(".mobile-search");
+        if ($('#tophead>.c-page').find('.mobile-menu').length > 0) {
+            $('#tophead>.c-page').remove('.mobile-menu');
+            $('#tophead>.c-page').remove('.mobile-search');
         }
     }
 });
 $(window).resize(function () {
     if ($(window).width() <= 768) {
         $('#tophead>.c-page').prepend($('header>.c-page>.logo'));
-        if ($("#tophead>.c-page").find(".mobile-menu").length > 0) {
+        if ($('#tophead>.c-page').find('.mobile-menu').length > 0) {
             return true
         } else {
-            var toggle = $("<span class='toggle-menu' />")
+            var toggle = $('<div class="toggle-menu" />')
+                .append('<span class="toggle-icon" />')
             $('#tophead>.c-page').append(toggle);
-            var search = $("<span class='mobile-search' />")
+            var search = $('<span class="mobile-search" />')
             $('#tophead>.c-page').append(search);
-            var menu = $("<div class='mobile-menu' />")
-                .append("<div class='mobile-list'></div>")
+            var menu = $('<div class="mobile-menu" />')
+                .append('<div class="mobile-list"></div>')
             $('#tophead>.c-page').append(menu);
 
-            $(".mobile-menu").append($("<span class='toggle-menu active' />"))
-            $("#tophead>.c-page>.menu").clone().appendTo($(".mobile-menu"))
-            $("#tophead>.c-page>.region").clone().appendTo($(".mobile-menu"))
-            $(".mobile-list").append("<div class='mobile-categ'></div>")
-            $(".mobile-list").append("<div class='mobile-items'></div>")
-            $("header>.c-page>nav>ul>li").clone().appendTo($('.mobile-categ'));
+            $('.mobile-menu').append($('<span class="toggle-menu active" />'))
+            $('#tophead>.c-page>.menu').clone().appendTo($('.mobile-menu'))
+            $('#tophead>.c-page>.region').clone().appendTo($('.mobile-menu'))
+            $('.mobile-list').append('<div class="mobile-categ"></div>')
+            $('.mobile-list').append('<div class="mobile-items"></div>')
+            $('header>.c-page>nav>ul>li').clone().appendTo($('.mobile-categ'));
         }
     } else {
         $('header>.c-page').prepend($('#tophead>.c-page>.logo'));
-        $(".mobile-menu").remove()
-        $(".toggle-menu").remove();
-        $(".mobile-search").remove();
+        $('.mobile-menu').remove()
+        $('.toggle-menu').remove();
+        $('.mobile-search').remove();
     }
 });
-$(document).on("click", ".toggle-menu", function () {
-    $(".mobile-menu").slideToggle()
+$(document).on('click', '.toggle-menu', function () {
+    $('.mobile-menu').slideToggle()
 })
